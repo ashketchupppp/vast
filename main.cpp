cc0631331853a91db74585158114265dc0e94411
ashketchupppp <computerchris101@gmail.com>
2024-09-05 18:40:37 +0100

    initial

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
