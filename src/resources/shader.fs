645922a802daa2c18e7462534ca473b2c39333d1
ashketchupppp <computerchris101@gmail.com>
2024-10-26 01:30:49 +0100

    include default shaders in build

--- src/resources/shader.fs
+++ src/resources/shader.fs
@@ -7,8 +7,7 @@ uniform float time;
 uniform sampler2D ourTexture;
 
 void main() {
-    // Multiply by vec2(1, -1) to flip the image the right way up
+    // We multiply by vec2(1, -1) to flip the image the right way up
     vec2 texColor = TexCoord * vec2(1, -1);
-    vec2 tanArg = texColor * texColor * texColor;
-    FragColor = texture(ourTexture, sin(tan(tanArg)));
+    FragColor = texture(ourTexture, tanArg);
 }
